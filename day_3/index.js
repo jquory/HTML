9dbe2cdd7511be6c5588c78ac07282439d490900
El <qorichsan@gmail.com>
2023-05-25 16:00:58 +0700

    day 3

--- day_3/index.js
+++ day_3/index.js
@@ -9,18 +9,63 @@
 //     }
 // });
 
-const bubbleSort = (arr) => {
-    let len = arr.length;
-    for (let i = 0; i < len - 1; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    return arr;
-};
+// const bubbleSort = (arr) => {
+//     let len = arr.length;
+//     for (let i = 0; i < len - 1; i++) {
+//         for (let j = 0; j < arr.length - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// };
 
-console.log(bubbleSort([12, 34, 56, 1, 23, 5, 6, 4, 89, 43, 45]));
+// const Mean = (array) => {
+//     let total = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         total += array[i] / array.length;
+//     }
+//     return console.log(total);
+// };
+// let arr = [12, 12, 34, 54];
+// Mean(arr);
+
+// const Median = (array) => {
+//     arr = array.sort((a, b) => a - b);
+//     let middleValue = 0;
+//     const middleIndex = Math.floor(array.length / 2);
+//     if (middleIndex % 2 === 0) {
+//         middleValue = array[middleIndex];
+//     } else {
+//         middleValue = array[middleIndex] + array[middleIndex - 1] / 2;
+//     }
+//     return console.log(middleValue);
+// };
+// let arr = [12, 3, 4, 5, 12, 23, 43, 123, 3];
+// console.log(arr.sort((a, b) => a - b));
+// Median(arr);
+
+// const Modus = (arr) => {
+//     let maxCount = 0;
+//     let modus = null;
+//     for (let i = 0; i < arr.length; i++) {
+//         let count = 0;
+//         for (let j = 0; j < arr.length; j++) {
+//             if (arr[i] === arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > maxCount) {
+//             maxCount = count;
+//             modus = arr[i];
+//         }
+//     }
+//     return console.log(modus);
+// };
+// let array = [9, 3, 4, 5, 10, 6, 7, 12, 10, 12, 10, 10, 10, 12, 12, 12, 12];
+
+// Modus(array);
