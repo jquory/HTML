18b1a400c03d4bf6918f9f23403daf5e6b0bec11
El <qorichsan@gmail.com>
2023-05-29 20:36:39 +0700

    Aku janji, klo aku bohong, aku janji lagi

--- day_5/sesi_1/index.js
+++ day_5/sesi_1/index.js
@@ -1,3 +1,4 @@
+// Get REST API
 // const angkaPertama = document.getElementById("angkaPertama");
 // const angkaKedua = document.getElementById("angkaKedua");
 // const btnTambah = document.getElementById("btnTambah");
@@ -27,18 +28,95 @@
 //     });
 // })();
 
+// ========================================================================= //
+
 let data = [
     {
-        name: "lala",
+        id: 1,
+        username: "lala",
+        address: "Jakarta",
     },
     {
-        name: "lili",
+        id: 2,
+        username: "lili",
+        address: "Mumbai",
     },
 ];
 
-const filter = (name) => {
-    return setTimeout(() => {
-        data.filter((d) => d.name === name);
-    }, 1000);
+const transaction = [
+    {
+        user_id: 1,
+        transaction: [
+            {
+                id: 1,
+                amount: 100,
+                status: "done",
+            },
+            {
+                id: 2,
+                status: "transfered",
+            },
+        ],
+    },
+    {
+        user_id: 2,
+        transaction: [
+            {
+                id: 1,
+                amount: 100,
+                status: "done",
+            },
+            {
+                id: 2,
+                status: "cancelled",
+            },
+        ],
+    },
+];
+
+const detailTransaction = [
+    {
+        id: 1,
+        productName: "black coffee",
+        quantity: 2,
+        totalAmount: 6000,
+    },
+    {
+        id: 1,
+        productName: "black tea",
+        quantity: 1,
+        totalAmount: 3000,
+    },
+];
+
+const authenticate = (username) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(data.filter((d) => d.username === username));
+        }, 100);
+    });
 };
-filter("lala");
+
+const getUserTransaction = (userId) => {
+    return new Promise((resolve, reject) => {
+        resolve(transaction.filter((d) => d.user_id == userId));
+    });
+};
+
+const getDetailTransaction = (idTransaction) => {
+    return new Promise((resolve, reject) => {
+        resolve(detailTransaction.filter((d) => d.id === idTransaction));
+    });
+};
+
+authenticate("lala")
+    .then((userData) => {
+        console.log(userData);
+        return getUserTransaction(userData[0].id);
+    })
+    .then((user) => {
+        console.log(user[0].transaction);
+        return getDetailTransaction(user[0].user_id);
+    })
+    .then((detailTransaction) => console.log(detailTransaction))
+    .catch((error) => console.log(error));
